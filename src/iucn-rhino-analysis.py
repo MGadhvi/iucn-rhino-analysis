5c9190285f3c5de355f74485281ea176fa13ba13
mgd <mgd@posteo.net>
2025-01-23 00:19:42 +0000

    Added full map to Rhino sightings

--- src/iucn-rhino-analysis.py
+++ src/iucn-rhino-analysis.py
@@ -16,9 +16,9 @@ def plot_species_distribution(gdf, save_path):
     plt.figure(figsize=(6, 6))
     gdf['SCI_NAME'].value_counts().plot(kind='pie', autopct='%1.1f%%')
     plt.title('Distribution of Species in Sample')
-    plt.ylabel('')  # Hide the y-label
+    plt.ylabel('') 
     plt.savefig(save_path)
-    plt.close()  # Close the figure to free memory
+    plt.close() 
 
 def plot_sightings_count(gdf, save_path):
     """Plot the number of sightings per species in a bar chart."""
@@ -29,31 +29,36 @@ def plot_sightings_count(gdf, save_path):
     plt.title('Number of Sightings per SCI_NAME')
     plt.xlabel('Species Name')
     plt.ylabel('Number of Sightings')
-    plt.xticks(rotation=45)  # Rotate x-axis labels for better readability
+    plt.xticks(rotation=45) 
     plt.tight_layout()
     plt.savefig(save_path)
-    plt.close()  # Close the figure to free memory
+    plt.close() 
 
-def plot_map(gdf, save_path):
-    """Plot a map of sightings."""
-    plt.figure(figsize=(20, 20))  # Adjust the figure size as needed
+
+def plot_map(gdf, save_path, world_shapefile_path):
+    """Plot a map of sightings with a grey background."""
+    plt.figure(figsize=(20, 20)) 
+
+    world = gpd.read_file(world_shapefile_path)  
+
+    world.boundary.plot(ax=plt.gca(), color='grey', linewidth=0.5)  
+    
     ax = gdf.plot(column='SCI_NAME',  
                    cmap='viridis',  
                    legend=True,  
-                   edgecolor='black')  
+                   edgecolor='black', 
+                   ax=plt.gca())
 
     plt.title('Map from Shapefile')
     plt.xlabel('Longitude')
     plt.ylabel('Latitude')
     plt.tight_layout()
     plt.savefig(save_path)
-    plt.close()  # Close the figure to free memory
+    plt.close() 
 
-# Main execution
 if __name__ == "__main__":
-    # Create the visualisations directory if it doesn't exist
-    os.makedirs('../visualisations', exist_ok=True)  # Adjusted path to go up one level
+    os.makedirs('../visualisations', exist_ok=True)  
     gdf = read_dataset('../data/data_0.shp')
     plot_species_distribution(gdf, '../visualisations/species_distribution.png')
     plot_sightings_count(gdf, '../visualisations/sightings_count.png')
-    plot_map(gdf, '../visualisations/map_of_sightings.png')
+    plot_map(gdf, '../visualisations/map_of_sightings.png', '../../world-shape-files/ne_110m_admin_0_countries.shp')
